e109bf17d42b217fcf6b926f72cc301a61ec380f
Ubuntu <ubuntu@ip-172-31-132-237.ap-southeast-1.compute.internal>
2017-10-15 19:49:29 +0000

    initial commit

---
+++ create_table.js
@@ -0,0 +1,21 @@
+let db-config = require(__dirname+'./db-config.json');
+
+const mysql = require('mysql');
+const _ = require('lodash');
+
+const con = mysql.createConnection(db-config);
+
+const category = ["btc_idr","bch_idr","eth_idr","etc_idr","ltc_idr","waves_idr","xrp_idr","xzc_idr","bts_btc","doge_btc","eth_btc","ltc_btc","nxt_btc","xlm_btc","xem_btc","xrp_btc"];
+
+function createTable(category) {
+    console.log("[create table start]");
+    con.query("CREATE TABLE "+category+" (tid INT(8) unsigned NOT NULL PRIMARY KEY, amount FLOAT NOT NULL, price FLOAT NOT NULL, date INT(10) unsigned NOT NULL, type VARCHAR(5) NOT NULL)",function(err,result) {
+        if(err) {
+            console.log("!!! error while creating table");
+        } else {
+            console.log("... succesfully inserted");
+        }
+    });
+}
+
+_.map(category,createTable);
---
+++ index.js
@@ -0,0 +1,35 @@
+let db-config = require(__dirname+'./db-config.json');
+
+const mysql = require('mysql');
+const axios = require('axios');
+const _ = require('lodash');
+
+const category = ["btc_idr","bch_idr","eth_idr","etc_idr","ltc_idr","waves_idr","xrp_idr","xzc_idr","bts_btc","doge_btc","eth_btc","ltc_btc","nxt_btc","xrp_btc"];
+
+var con = mysql.createConnection(db-config);
+
+function fetchNstore(category) {
+    console.log("[fetch start] __ " + category);
+        axios.get("https://vip.bitcoin.co.id/api/"+category+"/trades").then(res => {
+            const data = res.data;
+            const data_values = _.reduce(data,function(result,item){
+                result.push(_.values(item));
+                return result;
+            },[]);
+
+            con.query("INSERT IGNORE INTO "+category+" (date,price,amount,tid,type) VALUES?",[data_values],function(err,result) {
+                if(err) {
+                    console.log("!!! error while inserting... " + category);
+                    console.log(err);
+                } else {
+                    console.log(result);
+                    console.log("... succesfully insert " + data_values.length + " items" );
+                }
+                console.log("[store end] __ " + category);
+
+            });
+        });
+};
+
+//_.map(category,fetchNstore);
+const intervalObj = setInterval( () => { _.map(category,fetchNstore) }, 10 * 1000);
